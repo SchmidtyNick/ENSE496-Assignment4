056ba9c8738361398d22d9c4db27aac2bcdf0e5f
Nicolas Achter <nicolas_achter@hotmail.com>
2020-03-12 09:12:38 -0600

    Crerated PrimeBankGenerator

---
+++ PrimeBankGenerator/PrimeBankGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace ENSE496_A4
+{
+    class PrimeBankGenerator
+    {
+        static void Main(string[] args)
+        {
+            PrimeBankGenerator p = new PrimeBankGenerator();
+            p.primeGenerator();
+        }
+
+
+        public void primeGenerator()
+        {
+            BigInteger p = 13771193; //starting prime
+            int i = 1;  //prime counter
+            StreamWriter sw = new StreamWriter("E:/Documents/GitHub/ENSE496-Assignment4/PrimeBankCont.txt");  //prime bank file path
+            Stopwatch stopwatch = new Stopwatch();  //timer
+            long dur = 0;   //timer duration
+
+            using (sw)
+                //run until the time to get the a prime takes at least 3600000ms or 1hr
+                while (dur < 3600000)
+                {
+                    stopwatch.Start();      //start timer
+                    
+                    //increment p and check if it is prime
+                    while (!isPrime(p))
+                        p++;
+                    sw.Write(p + ",");  //output prime
+                    
+                    stopwatch.Stop();       //stop timer
+                    dur = stopwatch.ElapsedMilliseconds;
+                    stopwatch.Reset();
+
+                    Console.WriteLine("Prime " + i + ": " + p + " Time: " + dur + "ms");
+                    Console.WriteLine("");
+
+                    i++;
+                    p++;
+                }
+        }
+
+        //Compares the bytes of 2 files
+        public bool isPrime(BigInteger n)
+        {
+            if (n <= 1)
+                return false;
+            for (var i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+    }
+
+}
+
+
