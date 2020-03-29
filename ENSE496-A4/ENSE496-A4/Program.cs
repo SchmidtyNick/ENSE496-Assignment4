a33c660274935989dd03c42fcc7902203520a3ee
SchmidtyNick <32582247+SchmidtyNick@users.noreply.github.com>
2020-03-29 14:24:49 -0600

    Nicks side code comments

--- ENSE496-A4/ENSE496-A4/Program.cs
+++ ENSE496-A4/ENSE496-A4/Program.cs
@@ -14,11 +14,11 @@ namespace ENSE496_A4
         static void Main(string[] args)
         {
             Program p = new Program();
-            int seed = 802797117;
+            int seed = 802797117; //Seed created using Requirement 3
             Console.Write(" The value of the seed is: ");
             Console.WriteLine(seed);
-            p.RandomGenerator();
-            Console.WriteLine("Creating File OutputAlice.txt");
+            p.RandomGenerator(); //Generating a list of random integers and loading them into the file "OutputAlice.txt"
+            Console.WriteLine("Creating File OutputAlice.txt"); 
             Console.Write("The two files, OutputAlice and OutputBob are equal: ");
           bool FileCompare = FileEquals("C:/Users/Nickolas/Documents/GitHub/ENSE496-Assignment4/ENSE496-A4/ENSE496-A4/OutputAlice.txt",
              "C:/Users/Nickolas/Documents/GitHub/ENSE496-Assignment4/ENSE496-A4/ENSE496-A4/OutputBob.txt");
@@ -41,7 +41,7 @@ namespace ENSE496_A4
             }
         }
         //Compares the bytes of 2 files
-        static bool FileEquals(String fPath1, String fPath2)
+        static bool FileEquals(String fPath1, String fPath2) //File Comparision function
         {
             byte[] file1 = File.ReadAllBytes(fPath1); //File 1
             byte[] file2 = File.ReadAllBytes(fPath2); //File 2
@@ -54,7 +54,7 @@ namespace ENSE496_A4
             }
             return true;
         }
-        public void DHAlgoroith()
+        public void DHAlgoroith() //Deffie-Hellman algorithm to find a shared key to be used as Seed
         {
             BigInteger P, g, a, b, x, y, sa, sb;
             P = 1000000007; //Prime number P=1'000'000'000'007
@@ -71,7 +71,7 @@ namespace ENSE496_A4
             Console.WriteLine(sb);
         }
 
-        public BigInteger PowerFunct(BigInteger a, BigInteger b, BigInteger P)
+        public BigInteger PowerFunct(BigInteger a, BigInteger b, BigInteger P) //Powerfunction to use BigInteger values instead of ulong
         {
             BigInteger temp = 1; //Begin with a temp variable set to 1
             if (b == 1) //Base case of b=1
